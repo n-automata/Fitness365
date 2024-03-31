4c049608187e2a0b6477d31c0d7c005b39cbf652
FlameAether <rtu3915@conestogac.on.ca>
2024-03-31 12:49:09 -0400

    Points screen and history screen added

--- App.js
+++ App.js
@@ -1,30 +1,46 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./screens/LoginScreen.js";
-import MainPage from "./screens/MainPage.js";
-import SignUpScreen from "./screens/SignUpScreen.js";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MainPage from './screens/MainPage';
+import PointsScreen from './screens/PointsScreen';
+import LoginScreen from './screens/LoginScreen';
+import SignUpScreen from './screens/SignUpScreen';
+import ExerciseHistoryScreen from './screens/ExcerciseHistoryScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainScreens = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Exercise" component={MainPage} />
+      <Tab.Screen name="Points" component={PointsScreen} />
+      <Tab.Screen name="History" component={ExerciseHistoryScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
+        <Stack.Screen
+          name="Home"
           component={LoginScreen}
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen 
-          name="SignUp" 
-          component={SignUpScreen} 
           options={{ headerShown: false }}
         />
-        <Stack.Screen 
-          name="Main" 
-          component={MainPage} 
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name="Main"
+          component={MainScreens}
+          options={{ headerShown: false }}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
