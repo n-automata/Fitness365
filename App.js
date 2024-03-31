1c24b63dd3606904b1ef3724263dc1b70db10944
FlameAether <rtu3915@conestogac.on.ca>
2024-03-31 17:25:04 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -6,21 +6,46 @@ import MainPage from './screens/MainPage';
 import PointsScreen from './screens/PointsScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
-import ExerciseHistoryScreen from './screens/ExcerciseHistoryScreen';
+import HistoryScreen from './screens/HistoryScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+
+// create a bottom tab navigator with icons 
 const MainScreens = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Exercise') {
+            iconName = focused ? 'fitness' : 'fitness-outline'; 
+          } else if (route.name === 'Points') {
+            iconName = focused ? 'star' : 'star-outline'; 
+          } else if (route.name === 'History') {
+            iconName = focused ? 'time' : 'time-outline'; 
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarActiveTintColor="blue"
+      tabBarInactiveTintColor="gray"
+      tabBarStyle={{
+        display: 'flex',
+      }}
+    >
       <Tab.Screen name="Exercise" component={MainPage} />
       <Tab.Screen name="Points" component={PointsScreen} />
-      <Tab.Screen name="History" component={ExerciseHistoryScreen} />
+      <Tab.Screen name="History" component={HistoryScreen} />
     </Tab.Navigator>
   );
 };
 
+// assign the main screens 
 export default function App() {
   return (
     <NavigationContainer>
@@ -40,7 +65,6 @@ export default function App() {
           component={MainScreens}
           options={{ headerShown: false }}
         />
-
       </Stack.Navigator>
     </NavigationContainer>
   );
